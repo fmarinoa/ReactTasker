c8aa9588143aa567ee7143724f3219cb9ee43108
Francoedson <franco.edson.18014@gmail.com>
2023-08-29 01:26:45 -0500

    second commit

--- ReactTasker/ClientApp/src/App.js
+++ ReactTasker/ClientApp/src/App.js
@@ -25,7 +25,7 @@ const App = () => {
   const formatDate = (string) => {
     let options = { year: 'numeric', month: 'long', day: 'numeric' };
     let fecha = new Date(string).toLocaleDateString("es-PE", options);
-    let hora = new Date(string).toLocaleDateString();
+    let hora = new Date(string).toLocaleTimeString();
     return fecha + " | " + hora
   }
 
